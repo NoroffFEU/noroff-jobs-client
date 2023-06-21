85f9bb851c9179153c255cb81802c5c3e6adaefc
Marius Agerup <Marius.schei@outlook.com>
2023-06-21 23:52:38 +0200

    Created the apiCall function and added comments, Also created example file for understanding how to use the apiCall function

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import { getProducts } from './api-lib/example.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -22,3 +23,5 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+getProducts();
